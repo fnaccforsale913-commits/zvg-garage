16f9931949498431cdcd7d4788d231c6bcdfaeb1
fnaccforsale913-commits <fnaccforsale913@gmail.com>
2026-07-23 10:38:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,15 +18,3 @@ window.addEventListener('scroll', () => {
         }
     });
 });
-
-document.getElementById('bookingForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const phone = document.getElementById('phone').value;
-    const service = document.getElementById('service-select').value;
-    
-    const textMessage = "Salut ZVG Garage! Numele meu este " + name + ". Doresc o programare pentru serviciul: " + service + ". Numarul meu de contact este: " + phone;
-    
-    // Folosim link-ul oficial API pentru a evita eroarea pe iPhone
-    window.location.href = "https://whatsapp.com" + encodeURIComponent(textMessage);
-});
